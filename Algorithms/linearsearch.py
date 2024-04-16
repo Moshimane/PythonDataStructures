6d94c74da7a60e8d794dca0cac64c6bce6bdc144
Moshimane <joy.moshimane@gmail.com>
2024-04-16 23:52:11 +0200

    algorithms linearsearch

---
+++ Algorithms/linearsearch.py
@@ -0,0 +1,5 @@
+def search(ls, key):
+    for i in ls:
+        if i == key:
+            return True
+    return False
---
+++ Algorithms/test_linearsearch.py
@@ -0,0 +1,4 @@
+import linearsearch
+
+print(linearsearch.search([3,6,8,10,15], 8))
+print(linearsearch.search([3,6,8,10,15], 12))
